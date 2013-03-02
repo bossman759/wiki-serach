6081ca7334a0eb50be7508add8189deb343e41f2
bossman759 <bossman759@gmail.com>
2013-03-01 20:19:56 -0500

    Search.py UPDATE
    Added help for assistance

--- search.py
+++ search.py
@@ -8,12 +8,18 @@ import BeautifulSoup
 
 con = lite.connect('test.db')
 t = raw_input("Search: ")
+if("HELP" in t):
+    print "**HELP**"
+    
+    print "Search: Enter a search term to receive results."
+
+    print "**HELP**"
+    
+else:
+    print "**Type HELP for assistance.**"
+    
 with con:
     cur = con.cursor()    
-    #cur.execute("CREATE TABLE Websites(Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Url String)")
-    #cur.execute("INSERT INTO Websites VALUES(NULL, 'Mega', 'http://mega.co.nz/')")
-    #SQL = "SELECT * FROM Things WHERE Name='Samsung Galaxy S3 T-Mobile'"    
-    #SQL = "SELECT * FROM Things WHERE Name='Samsung Galaxy S3 T-Mobile'"
     
     cur.execute("select * from Dict where Def LIKE ? OR word LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
     rows = cur.fetchall()
