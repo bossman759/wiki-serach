c258a2667d19e006d3247fea7d2f584573ebdfd4
bossman759 <bossman759@gmail.com>
2013-03-02 19:52:03 -0500

    Search.py now with history

--- search.py
+++ search.py
@@ -15,6 +15,8 @@ if("HELP" in t):
     print "**HELP**"
     
     print "Search: Enter a search term to receive results."
+    
+    print "History: Enter HISTORY to see recent searches."
 
     print "**HELP**"
     
@@ -24,32 +26,46 @@ else:
 
 with con:
     #create our cursor
-    cur = con.cursor()    
-    #SQL query for search through Dict table
-    cur.execute("select * from Dict where Def LIKE ? OR word LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
-    rows = cur.fetchall()
+    cur = con.cursor()
     
-    for row in rows:
-        #printing second & third column(See tuples)
-        print row[1]
+    if("HISTORY" in t):
+        cur.execute("select * from HISTORY")
+        rows = cur.fetchall()
         
-        print row[2]
-        #SQL query for search through Websites table
-    cur.execute("SELECT * FROM Websites WHERE Name LIKE ? OR Url LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
-    rows = cur.fetchall()
-    
-    for row in rows:
-        #printing second & third column(See tuples)
-        print row[1]
+        for row in rows:
+            
+            print row[1]
+    else: 
+            
+        cur.execute("INSERT INTO History VALUES(NULL, ?)", (t,))        
+               
+        #SQL query for search through Dict table
+        cur.execute("select * from Dict where Def LIKE ? OR word LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
+        rows = cur.fetchall()
         
-        print row[2]
-        #SQL query for search through Things table    
-    cur.execute("select * from Things where Name like ? LIMIT 12", ('%'+t+'%',))
-    rows = cur.fetchall()
-    
-    for row in rows:
-        #printing second & third column(See tuples)
-        print row[1]
+        for row in rows:
+            #printing second & third column(See tuples)
+            print row[1]
+            
+            print row[2]
+            
+            #SQL query for search through Websites table
+        cur.execute("SELECT * FROM Websites WHERE Name LIKE ? OR Url LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
+        rows = cur.fetchall()
         
-        print row[2]
-    
+        for row in rows:
+            #printing second & third column(See tuples)
+            print row[1]
+            
+            print row[2]
+            #SQL query for search through Things table    
+        cur.execute("select * from Things where Name like ? LIMIT 12", ('%'+t+'%',))
+        rows = cur.fetchall()
+        
+        for row in rows:
+            #printing second & third column(See tuples)
+            print row[1]
+            
+            print row[2]
+            
+        
